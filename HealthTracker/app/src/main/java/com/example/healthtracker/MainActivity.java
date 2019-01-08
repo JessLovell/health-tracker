d985d67170f9a6eb1631bab3380b4e24d15c476b
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-01-08 10:18:30 -0800

    complete finger exercises

--- HealthTracker/app/src/main/java/com/example/healthtracker/MainActivity.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.healthtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fingerExercise(View v){
+
+        TextView textview = findViewById(R.id.textView);
+        TextView message = findViewById(R.id.textView2);
+
+
+        int click = Integer.parseInt(textview.getText().toString());
+        click++;
+
+        if (click % 50 == 0 && click != 0){
+            message.setText("Look at those slim fingers!");
+            textview.setText(click);
+        } else if (click % 10 == 0){
+            message.setText("The fat is melting away!");
+            textview.setText(click);
+        } else{
+            message.setText("Get those thumbs clicking!");
+            textview.setText(click);
+        }
+
+        System.out.println("click");
+
     }
 }
