00868c66e85600086c13202164c5ecdb7f605988
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-01-16 10:30:04 -0800

    add to db testing

--- HealthTracker/app/src/androidTest/java/com/example/healthtracker/JournalTest.java
+++ HealthTracker/app/src/androidTest/java/com/example/healthtracker/JournalTest.java
@@ -4,12 +4,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.room.Room;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
@@ -50,5 +54,28 @@ public class JournalTest {
         //check that the page is refreshed on button click
         onView(withId(R.id.button7)).perform(click());
         intended(hasComponent(Journal.class.getName()));
+    }
+
+    @Test
+    public void addToDatabase() {
+
+        for (int i = 1; i < 4; i++){
+
+            onView(withId(R.id.editText4)).perform(clearText(),
+                    typeText("Test Title " + i));
+            onView(withId(R.id.editText5)).perform(clearText(),
+                    typeText(i + ""));
+            onView(withId(R.id.editText6)).perform(clearText(),
+                    typeText("Test Description " + i));
+            onView(withId(R.id.button7)).perform(click());
+
+            ExerciseDatabase testDb =  Room.databaseBuilder(getApplicationContext(),
+                    ExerciseDatabase.class, "exercise_journal").allowMainThreadQueries().build();
+
+            Exercise testAdd = testDb.exerciseDao().getLast();
+            assertEquals( "Test Title " + i, testAdd.title);
+            assertEquals( "Test Description " + i, testAdd.description);
+            assertEquals( i + "", testAdd.quantity);
+        }
     }
 }
