81c0191f2eb356a15e86ace2c79b2f36ea510bd7
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-01-16 13:08:07 -0800

    fix image carousel

--- HealthTracker/app/src/main/java/com/example/healthtracker/MainActivity.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/MainActivity.java
@@ -6,43 +6,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import com.synnapps.carouselview.CarouselView;
-import com.synnapps.carouselview.ImageListener;
 
 
 public class MainActivity extends AppCompatActivity {
 
     //FINGER EXERCISE
-    private int click = 0;
-
-    //CAROUSEL
-    CarouselView carouselView;
-    int[] sampleImages = {R.drawable.exercise, R.drawable.sleep, R.drawable.vegggies, R.drawable.water};
-    String[] imageText = {"Exercise is important!",
+    private int imageIndex = 0;
+    private int[] sampleImages = {R.drawable.exercise, R.drawable.sleep, R.drawable.vegggies, R.drawable.water};
+    private String[] imageText = {"Exercise is important!",
             "Get Lots of sleep so you can be the most productive.",
             "Eat fruits and veggies to get more energy.", "Stay hydrated!" };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //for the carousel
-        carouselView = (CarouselView) findViewById(R.id.carouselView);
-        carouselView.setPageCount(sampleImages.length);
-        carouselView.setImageListener(imageListener);
     }
 
-    //directions from carousel Library
-    ImageListener imageListener = new ImageListener() {
-        @Override
-        public void setImageForPosition(int position, ImageView imageView) {
-            TextView textView = (TextView) findViewById(R.id.textView);
-            textView.setText(imageText[position]);
+    public void imageNextClicked(View v){
 
-            imageView.setImageResource(sampleImages[position]);
-        }
-    };
+        ImageView image = findViewById(R.id.imageView);
+        image.setImageResource(sampleImages[imageIndex++ % sampleImages.length]);
+
+        TextView text = findViewById(R.id.textView);
+        text.setText(imageText[imageIndex++ % sampleImages.length]);
+
+        TextView counter = findViewById(R.id.textView7);
+        counter.setText(imageIndex++ % sampleImages.length + " of " + sampleImages.length);
+    }
+
+    public void imageBackClicked(View v){
+
+        ImageView image = findViewById(R.id.imageView);
+        image.setImageResource(sampleImages[imageIndex-- % sampleImages.length]);
+
+        TextView text = findViewById(R.id.textView);
+        text.setText(imageText[imageIndex-- % sampleImages.length]);
+    }
 
     public void stopWatchClicked(View v) {
         Intent stopWatch = new Intent(this, StopWatch.class);
