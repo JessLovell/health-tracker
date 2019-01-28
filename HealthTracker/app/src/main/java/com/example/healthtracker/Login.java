83eb23b6fc53509074a73d6eac82b5e1017cd79b
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-01-27 17:51:47 -0800

    Got A COOKIE

--- HealthTracker/app/src/main/java/com/example/healthtracker/Login.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/Login.java
@@ -2,11 +2,15 @@ package com.example.healthtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -14,15 +18,24 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.net.CookieHandler;
+import java.net.CookieManager;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
+    SharedPreferences sharedPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        CookieManager cookieManager = new CookieManager();
+        CookieHandler.setDefault(cookieManager);
+
+        sharedPref= this.getSharedPreferences(getString(R.string.logged_in_user), Context.MODE_PRIVATE);
     }
 
     public void onLoginSubmit(View v){
@@ -47,7 +60,7 @@ public class Login extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.i("Login", "GOT RESPONSE + " + response);
+                        Log.i("Login", "GOT RESPONSE " + response);
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -62,6 +75,24 @@ public class Login extends AppCompatActivity {
                 params.put("password", password);
                 return params;
             }
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                // since we don't know which of the two underlying network vehicles
+                // will Volley use, we have to handle and store session cookies manually
+                Log.i("response",response.headers.toString());
+//                Map<String, String> responseHeaders = response.headers;
+//                String rawCookies = responseHeaders.get("Set-Cookie");
+
+                //save to shared pref
+//                SharedPreferences.Editor editor = sharedPref.edit();
+//                editor.putString(getString(R.string.Stored_cookie), rawCookies);
+//                editor.commit();
+//
+//
+//                Log.i("cookies", sharedPref.getString(getString(R.string.Stored_cookie), "cocoloco"));
+                return super.parseNetworkResponse(response);
+            }
+
         };
 
         // Add the request to the RequestQueue.
